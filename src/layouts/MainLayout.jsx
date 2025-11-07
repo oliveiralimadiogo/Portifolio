de5e4bd15606029a5f403a570951e0a7da3e69d3
Diogo Oliveira Lima <limaa.diogi@gmail.com>
2025-11-07 02:33:24 -0300

    BugFix: Resolvendo problema para dar deploy no vercel

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavBar.jsx";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router";
